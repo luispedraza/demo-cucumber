a0dd4aa95d0be928e092ed74afaac56eab24d969
Luis <luispedraza@gmail.com>
2023-05-24 17:35:54 +0200

    agregada una prueba para la multiplicación

--- features/steps/suma.steps.ts
+++ features/steps/suma.steps.ts
@@ -3,22 +3,30 @@ import { Calculator } from '../../src/calculator';
 import { expect } from 'chai'
 
 let calculator: Calculator
-let sum: number
+let result: number
 
-Given("Una calculadora", () => {
+Given("Tengo una calculadora", () => {
   calculator = new Calculator()
 });
 
-When("Se ingresan {int} y {int}", (num1: number, num2: number) => {
+When("Se suman {int} y {int}", (num1: number, num2: number) => {
   // When('Se ingresan {int} y {float}', function (int, float) {
   // When('Se ingresan {float} y {int}', function (float, int) {
   // When('Se ingresan {float} y {float}', function (float, float2) {
   // Write code here that turns the phrase above into concrete actions
-  sum = calculator.sum(num1, num2)
+  result = calculator.sum(num1, num2)
 });
 
-Then("El resultado debe ser {int}", (result: number) => {
+When("Se multiplican {int} y {int}", (num1: number, num2: number) => {
+  // When('Se ingresan {int} y {float}', function (int, float) {
+  // When('Se ingresan {float} y {int}', function (float, int) {
+  // When('Se ingresan {float} y {float}', function (float, float2) {
+  // Write code here that turns the phrase above into concrete actions
+  result = calculator.multi(num1, num2)
+});
+
+Then("El resultado debe ser {int}", (resultExpected: number) => {
   // Then('El resultado debe ser {float}', function (float) {
   // Write code here that turns the phrase above into concrete actions
-  expect(sum).to.be.equal(result)
+  expect(result).to.be.equal(resultExpected)
 });
--- features/suma.feature
+++ features/suma.feature
@@ -1,8 +1,21 @@
-Feature: Suma de dos enteros
+Feature: Calculadora
 
 Scenario: Suma de dos enteros positivos
     Given Tengo una calculadora
-    When Se ingresan los numeros 6 y 4
+    When Se suman 6 y 4
     Then El resultado debe ser 10
 
-    
+Scenario: Suma de dos enteros negativos
+    Given Tengo una calculadora
+    When Se suman <num1> y <num2>
+    Then El resultado debe ser <num3>
+
+    Examples:
+        | num1  | num2 | num3   |
+        | 1     | 2     | 3     |
+        | 2     | 5     | 7     |
+
+Scenario: Multiplicación de dos enteros positivos
+    Given Tengo una calculadora
+    When Se multiplican 5 y 6
+    Then El resultado debe ser 30
